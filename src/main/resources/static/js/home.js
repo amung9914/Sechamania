34b3bbff1636ca87c99c78c2102782d5171db018
amung9914 <rlatjdud511@naver.com>
2024-01-19 12:44:57 +0900

    feat: 기상청 api 연결
    정상 출력 완료

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded',function (){
 
-
-    const weatherImg = document.getElementById("weather_img");
-    let city = null;
     let lat = null;
     let lon = null;
     let isLogin = localStorage.getItem("access_token");
     // 로그인 된 상태
     if(isLogin){
-        let API_KEY = document.getElementById("openweather_key").value;
+        let API_KEY = document.getElementById("weather_key").value;
         function success(response){
-            city = response.data.city;
+            document.getElementById("city").innerText = response.data.city;
             lat = response.data.lat;
             lon = response.data.lon;
-            getWeather(lat,lon);
+            let grid = dfs_xy_conv("toXY",lat,lon);
+
+            getWeather(grid.x,grid.y);
         }
 
         function fail() {
@@ -21,28 +20,83 @@ document.addEventListener('DOMContentLoaded',function (){
         }
         httpRequestWtihTokenAndResponse("GET","/memberInfo",null,success,fail);
 
-        const getWeather = (lat, lon) => {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`)
-                .then((response)=>{
-                    return response.json();
-                })
-                .then((data) =>{
-                    console.log(data);
-                    console.log("날씨는 ",data["weather"][0]["description"],"입니다.");
-                    console.log(("현재 온도는 ",data["main"]["temp"]));
-                    console.log("최저 기온은 ",data["main"]["temp_min"],"입니다.");
-                    console.log("최고 기온은 ",data["main"]["temp_max"],"입니다.");
-                })
+        /* 기상청 날씨 api START */
+        /*
+        *  날짜 변수
+        */
+        let now = new Date();
 
+        let year = now.getFullYear();
+        let month = ('0' + (now.getMonth() + 1)).slice(-2);
+        let day = ('0' + now.getDate()).slice(-2);
 
+        /*
+        * 시간 변수
+        */
+        let hours = now.getHours();
+        let minutes = ('0' + now.getMinutes()).slice(-2);
+        let isYesterday = false;
+
+        /*
+        * - Base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
+        * - API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
+        */
+        const baseTimeArr = [2,5,8,11,14,17,20,23];
+
+        function time(){
+            // 2시 이전인 경우 어제 23시로 호출함
+            if(hours<baseTimeArr[0]){
+                isYesterday = true;
+                return '2300';
+            }
+            for (let i = 1; i < baseTimeArr.length; i++) {
+                if(hours < baseTimeArr[i]){
+                    return ('0' + baseTimeArr[i-1]).slice(-2)+'00';
+                }else if(hours === baseTimeArr[i] && minutes > '10'){
+                    return ('0' + baseTimeArr[i]).slice(-2)+'00';
+                }
+            }
         }
 
+        function calcDateString(isYesterday){
+            if(isYesterday){
+                let yesterday = new Date(now);
+                yesterday.setDate(now.getDate()-1);
+                day = ('0' + yesterday.getDate()).slice(-2);
+                return year+ month + day;
+            }else{
+                return year+ month + day;
+            }
+        }
+
+        const getWeather = (gridX,gridY) => {
+            let url = new URL("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst");
+            let params = {
+                serviceKey: `${API_KEY}`,
+                pageNo: '1',
+                numOfRows: '112',
+                dataType: 'JSON',
+                base_time: time(),
+                base_date: calcDateString(isYesterday),
+                nx: gridX,
+                ny: gridY
+            };
+            url.search = new URLSearchParams(params).toString();
+            fetch(decodeURI(url))
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                   weather_data(data);
+                }).catch(err => {
+                // error 처리
+                console.log('Fetch Error', err);
+            });
+        };
+        /* 기상청 날씨 api END */
 
 
-
-
-
-    } // if weatherImg END
+    } // 로그인된 상태 END
 
 
 });
---
+++ src/main/resources/static/js/weather.js
@@ -0,0 +1,107 @@
+/**
+ * 기준시간 기준 10시간 이후에 data까지 확인 가능
+ *  10시간 이후 data로 forecate 출력
+ */
+function calcTime(time_weather,state){
+    if ('06' < time_weather && time_weather < '12') {
+        return '내일 오전 : '+state;
+    }else if('12' <= time_weather && time_weather < '20'){
+        return '오늘 오후 : '+state;
+    }else{
+        return '오늘 밤 : '+state;
+    }
+}
+
+/**
+ * SKY: 하늘 상태 맑음(1), 구름많음(3), 흐림(4)
+ * PTY:강수형태 없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4)
+ */
+function weather_data(data){
+    console.log(data);
+    const currentSKY = data.response.body.items.item[5].fcstValue;
+    const currentPTY = data.response.body.items.item[6].fcstValue;
+    document.getElementById("sky").innerText = make_weather(currentSKY,currentPTY);
+    const currentTMP = data.response.body.items.item[0].fcstValue;
+    document.getElementById("temp").innerText = '현재 기온 '+currentTMP+'°C';
+
+    const after10HourSKY = data.response.body.items.item[102];
+    const time_weather = after10HourSKY.fcstTime.slice(0, -2);
+    const after10HourPTY = data.response.body.items.item[103];
+    document.getElementById("forecast").innerText =
+        calcTime(time_weather,make_forecastWeather(after10HourSKY.fcstValue,after10HourPTY.fcstValue));
+    document.getElementById("summary").innerText =
+        make_summary(currentTMP,currentPTY,after10HourPTY.fcstValue);
+}
+
+function make_weather(sky,pty){
+    const weatherImg = document.getElementById("weather_img");
+    switch(pty){
+        case '0':
+            if(sky==='1'){
+                return '맑음';
+            }else if(sky==='3'){
+                weatherImg.src = '/img/clouds.jpg';
+                return '구름많음';
+            }else{
+                weatherImg.src = '/img/brokenClouds.jpg';
+                return '흐림';
+            }
+        case '1':
+            weatherImg.src = '/img/rain.jpg';
+            return '비';
+        case '2':
+            weatherImg.src = '/img/rain.jpg';
+            return '비/눈';
+        case '3':
+            weatherImg.src = '/img/snow.jpg';
+            return '눈';
+        case '4':
+            weatherImg.src = '/img/rain.jpg';
+            return '소나기';
+    }
+}
+
+function make_forecastWeather(sky,pty){
+    switch(pty){
+        case '0':
+            if(sky==='1'){
+                return '맑음';
+            }else if(sky==='3'){
+                return '구름많음';
+            }else{
+                return '흐림';
+            }
+        case '1':
+            return '비';
+        case '2':
+            return '비/눈';
+        case '3':
+            return '눈';
+        case '4':
+            return '소나기';
+    }
+}
+
+// 강수 예보로 1차 판단 (PTY : 강수형태)
+// 맑은 날씨일 경우
+// 현재 기온 기준으로 차가 어는지 안어는지 판단
+function make_summary(currentTMP,pty,afterPty){
+    switch(pty){
+        case '0':
+            if(currentTMP >'1'&& afterPty==='0'){
+                return '오늘은 세차하기 좋은 날이에요';
+            }else if(currentTMP <='1'){
+                return '오늘은 차가 얼어버릴 수 있어요';
+            }else{
+                return '세차는 다음에 하는 게 어떨까요?'
+            }
+        case '1':
+            return '오늘은 자연세차의 날';
+        case '2':
+            return '세차는 다음 기회에';
+        case '3':
+            return '세차는 다음 기회에';
+        case '4':
+            return '오늘은 자연세차의 날';
+    }
+}
